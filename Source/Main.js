c7ee1edbc4e2d867a684671b56e485f3d6f3faf2
ShadowDev <teammicro2026@outlook.com>
2026-04-23 10:36:50 +0200

    Update Main.js

--- Source/Main.js
+++ Source/Main.js
@@ -1,1 +1,6 @@
-
+import { default as CameraPos } from "./Camera/CamPos.js";
+import { default as Gravity } from "./Scene/Gravity.js";
+import { default as SceneViewport } from "./Scene/SceneViewport.js";
+import { default as Enum } from "./Core/Enum.js";
+import { default as Math } from "./Core/Math.js";
+import { default as Position } from "./Core/POS.js";
